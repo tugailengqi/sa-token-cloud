f982f11baa3e1421ae00cfbfe69072106d3c8972
lengqi <1982182894@qq.com>
2021-05-06 21:20:39 +0800

    不提交target文件

--- sa-token-auth/src/main/java/com/lengqi/cloud/token/controller/AuthController.java
+++ sa-token-auth/src/main/java/com/lengqi/cloud/token/controller/AuthController.java
@@ -1,19 +1,26 @@
 package com.lengqi.cloud.token.controller;
 
+import com.lengqi.cloud.token.service.AuthService;
 import com.lengqi.common.result.ResultCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 @RequestMapping("/auth")
 @Slf4j
 public class AuthController {
 
+    @Resource
+    private AuthService authService;
+
     //登录
     @PostMapping("/doLogin")
-    public ResultCode doLogin(){
+    public ResultCode doLogin(String key,String password){
+
         return null;
     }
 
